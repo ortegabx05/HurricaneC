b35322cd92edfdaf5b4d6c2eeaa34a9c3fd519c2
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2024-02-04 03:26:22 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    int windSpeed;
+    char *cat;
+
+    printf("Enter wind speed: ");
+    scanf("%d", &windSpeed);
+
+    if (windSpeed >= 74 && windSpeed <= 95)
+        cat = "Category One Hurricane";
+    else if (windSpeed >= 96 && windSpeed <= 110)
+        cat = "Category Two Hurricane";
+    else if (windSpeed >= 111 && windSpeed <= 129)
+        cat = "Category Three Hurricane";
+    else if (windSpeed >= 130 && windSpeed <= 156)
+        cat = "Category Four Hurricane";
+    else
+        cat = "Not a Hurricane";
+
+    printf("%s\n", cat);
+
+    return 0;
+}
